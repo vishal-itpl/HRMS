de3c0f726e5acb5702ca72ad8bc54fcc11a77ba2
Vishal Kewalramani <vishal@itechnotree.com>
2023-08-07 14:22:35 +0530

    change

--- login1.php
+++ login1.php
@@ -18,18 +18,21 @@
 
 </head>
 
+
 <body class="hold-transition login-page">
   <div class="login-box">
     <div class="login-logo">
-        <b>HR Management System</b></a>
+      <b>HR Management System</b></a>
     </div>
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <form action="home.php" method="post">
+        <form action="" method="post">
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email" required>
+            <input type="email" class="form-control" placeholder="Email" name="email" required>
+
+
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -37,7 +40,7 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password" required>
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
@@ -46,23 +49,24 @@
           </div>
           <div class="row">
             <div class="col-12 text-center">
-              <button type="submit" class="btn btn-primary" onclick="window.location.href = 'home.php';">Login</button>
+              <button type="submit" class="btn btn-primary" >Login</button>
             </div>
           </div>
 
         </form>
 
-      
-      <div>
-        <p class="mt-3 text-center">
-          <a href="forgot-password.php">I forgot my password</a>
-        </p>
+
+        <div>
+          <p class="mt-3 text-center">
+            <a href="forgot-password.php">I forgot my password</a>
+          </p>
         </div>
-        
+
       </div>
 
     </div>
   </div>
+
 
 
   <!-- jQuery -->
@@ -73,4 +77,42 @@
   <script src="dist/js/adminlte.min.js"></script>
 </body>
 
-</html>
+
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hrms";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Validate credentials against the database
+    $query = "SELECT * FROM credentials WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        // Valid credentials, redirect to the dashboard
+        header("Location: home.php");
+        exit();
+    } else {
+        // Invalid credentials, show an error message
+        echo "<p class='login-box-msg' style='color: red;'>Login failed. Please check your credentials.</p>";
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+    
